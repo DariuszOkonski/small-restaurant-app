64f2880265e8420861122c2801d3d3aa93bb351a
Dariusz Okonski <darek200180@gmail.com>
2022-03-02 17:40:16 +0000

    build recipe component

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,7 +1,20 @@
 import './Recipe.css';
+import useFetch from './../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 export default function Recipe() {
+
+  const { id } = useParams();
+  const { data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      { error && <p className='error'>{error}</p>}
+      { isPending && <p className='loading'>Loading...</p>}
+
+      {
+        recipe && <h1>{recipe.title}</h1>
+      }
+    </div>
   )
 }
