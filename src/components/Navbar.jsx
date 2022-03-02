652a4de2f539979e6b452809c99da6d1ae5591f3
Dariusz Okonski <darek200180@gmail.com>
2022-03-02 15:51:35 +0000

    add styles to Navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <nav>
+            <Link to='/' className='brand'>
+                <h3>small restaurant app</h3>
+            </Link>
+
+            <Link to='/create'>Create Recipe</Link>
+        </nav>
+    </div>
   )
 }
