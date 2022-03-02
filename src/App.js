4ef4d0685dd239ded8a9ca2e4d317277e1e2c0ac
Dariusz Okonski <darek200180@gmail.com>
2022-03-02 15:32:09 +0000

    install react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Small restaurant</h1>
+
       </header>
     </div>
   );
