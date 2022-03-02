966bd820c226e97082e6743f905b6827ffd2ce9b
Dariusz Okonski <darek200180@gmail.com>
2022-03-02 17:27:50 +0000

    add RecipeList component

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import './RecipeList.css';
+
+export default function RecipeList({ recipes }) {
+    console.log(recipes)
+
+    return (
+        <div className='recipe-list'>
+            {
+                recipes.map(recipe => (
+                    <div key={recipe.id} className='card'>
+                        <h3>{ recipe.title }</h3>
+                        <p>{ recipe.cookingTime }</p>
+
+                        <div>
+                            {recipe.method.substring(0, 50)}...
+                        </div>
+
+                        <Link to={`/recipe/${recipe.id}`} >Cook This...</Link>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
