485d2b75f07732944dedcb56d4ad20c4ec03cdd1
Dariusz Okonski <darek200180@gmail.com>
2022-03-02 15:37:42 +0000

    setup all routes in app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Create from './pages/create/Create';
 import Home from './pages/home/Home';
@@ -8,6 +9,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+        <BrowserRouter>
+          <Switch>
+            
+            <Route exact path='/'>
+              <Home />
+            </Route>
+
+            <Route path='/create'>
+              <Create/>
+            </Route>
+
+            <Route path='/search'>
+              <Search/>
+            </Route>
+
+            <Route path='/recipe/:id'>
+              <Recipe/>
+            </Route>
+          </Switch>
+        </BrowserRouter>
 
       </header>
     </div>
