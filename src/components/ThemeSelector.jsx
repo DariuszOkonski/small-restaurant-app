9252b3cded8edd8c83a795247204c28efcead818
Dariusz Okonski <darek200180@gmail.com>
2022-03-04 16:54:54 +0000

    add mode state to ThemeProvider

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -1,22 +1,35 @@
-import { useTheme } from './../hooks/useTheme';
 import './ThemeSelector.css'
+import { useTheme } from './../hooks/useTheme';
+import modeIcon from './../assets/mode-icon.svg';
+
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-    const { changeColor } = useTheme();
+    const { mode, changeColor, changeMode } = useTheme();
 
-  return (
-    <div className='theme-selector'>
-        <div className="theme-buttons">
-            { themeColors.map(color => (
-                <div  
-                    key={color}
-                    onClick={() => changeColor(color)}
-                    style={{ background: color }}
+    const toggleMode = () => {
+        changeMode(mode === 'dark' ? 'light' : 'dark')
+    }
+
+    return (
+        <div className='theme-selector'>
+            <div className="mode-toggle">
+                <img
+                    onClick={toggleMode} 
+                    src={modeIcon} alt="icon" 
                 />
-            ))}
+            </div>
+
+            <div className="theme-buttons">
+                { themeColors.map(color => (
+                    <div  
+                        key={color}
+                        onClick={() => changeColor(color)}
+                        style={{ background: color }}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
