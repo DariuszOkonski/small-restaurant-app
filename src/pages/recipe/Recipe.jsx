24ab41c578403b5865b40e95f657fd163d2ef886
Dariusz Okonski <darek200180@gmail.com>
2022-03-07 14:30:47 +0000

    add functionality to get single recipe

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,13 +1,39 @@
 import './Recipe.css';
-import useFetch from './../../hooks/useFetch';
+// import useFetch from './../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import { useTheme } from './../../hooks/useTheme';
+import { useEffect, useState } from 'react';
+import { projectFirestore } from '../../firebase/config';
 
-export default function Recipe() {
-
+export default function Recipe() {  
   const { id } = useParams();
-  const { data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
   const { mode } = useTheme();
+  
+  const [data, setData] = useState(null)  
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore.collection('recipes').doc(id).get()
+      .then((doc) => {
+        if(!doc.exists) {
+            setIsPending(false);
+            setError('Could not find that recipe')
+        } else {
+          setIsPending(false);
+          setError(false);
+          setData(doc.data())
+        }
+
+      })
+      .catch(err => {
+        setError(err);
+      })
+
+  }, [id])
+  
 
   return (
     <div className={`recipe ${mode}`}>
@@ -15,15 +41,15 @@ export default function Recipe() {
       { isPending && <p className='loading'>Loading...</p>}
 
       {
-        recipe && (
+        data && (
           <>
-            <h2 className="page-title">{recipe.title}</h2>
-            <p>Takes {recipe.cookingTime} to cook.</p>
+            <h2 className="page-title">{data.title}</h2>
+            <p>Takes {data.cookingTime} to cook.</p>
 
             <ul>
-              { recipe.ingredients.map(ing => <li key={ing}>{ing}</li>) }
+              { data.ingredients.map(ing => <li key={ing}>{ing}</li>) }
             </ul>
-            <p className="method">{recipe.method}</p>
+            <p className="method">{data.method}</p>
           </>
         )
       }
