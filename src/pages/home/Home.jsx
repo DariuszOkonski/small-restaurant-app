2facdd491386816b5f8111fc6e24dd21dd0478de
Dariusz Okonski <darek200180@gmail.com>
2022-03-07 14:12:09 +0000

    add functionality to get data from firestore at Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,42 @@
 import './Home.css';
-import useFetch from './../../hooks/useFetch';
 import RecipeList from '../../components/RecipeList';
+import { projectFirestore } from '../../firebase/config';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  useEffect(() => {
+    setIsPending(true);
+    
+    projectFirestore.collection('recipes').get()
+      .then((snapshot) => {
+          if(snapshot.empty) {
+            setError('No recipies to load');
+            setIsPending(false);
+          
+          } else {
+            let results = [];
+            snapshot.docs.forEach(doc => {
+              results.push({ id: doc.id, ...doc.data() })
+            });
+
+            setIsPending(false);
+            setError(false);
+            setData(results);
+          }
+      })
+      .catch(err => {
+        setError(err.message);
+        setIsPending(false)
+      })
+
+  }, [])
+  
+
   return (
     <div className='home'>
       { error && <p className='error'>{error}</p>}
